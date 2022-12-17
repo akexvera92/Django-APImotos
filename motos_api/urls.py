515a4bb56d45965c98526cfbb529ced3eeaafd09
akexvera92 <javera573@misena.edu.com>
2022-12-17 12:41:45 -0500

    appBlog

---
+++ motos_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import MotoListApiView,MotoDetailApiView
+
+urlpatterns=[
+    path('',MotoListApiView.as_view(),name="Moto_list"),
+    path('<int:moto_id>/',MotoDetailApiView.as_view(),name="Moto_detail"),
+
+]
